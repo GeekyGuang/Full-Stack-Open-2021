0782d0d12adbe0803fe3913e03e8bc6d8b4497ed
TravisWongX <travis.x.wong@gmail.com>
2021-06-24 16:05:16 +0800

    jsx

---
+++ part1/src/App.js
@@ -0,0 +1,20 @@
+//import React from 'react'
+
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+
+    console.log('Hello from component')
+
+    return (
+        <div>
+            <p>Hello World, it is {now.toString()}</p>
+            <p>
+                {a} plus {b} is {a + b}
+            </p>
+        </div>
+    )
+}
+
+export default App
