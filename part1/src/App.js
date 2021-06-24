3ebae691cc52b2b280536466ee01c29e7e79f4ab
TravisWongX <travis.x.wong@gmail.com>
2021-06-24 16:26:31 +0800

    props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,21 @@
 //import React from 'react'
-const Hello = () => (
-    <div>
-        <p>Hello World</p>
-    </div>
+const Hello = (props) => (
+    <>
+        <p>Hello {props.name}, you are {props.age} years old</p>
+    </>
 )
 
-const App = () => (
-    <div>
-        <h1>Greetings</h1>
-        <Hello />
-        <Hello />
-        <Hello />
-    </div>
-)
+const App = () => {
+    const name = 'Peter'
+    const age = 10
+
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} />
+        </>
+    )
+}
 
 export default App
