16e2ec5d9eb274284467fd5f018e3aefa7ee4171
TravisWongX <travis.x.wong@gmail.com>
2021-07-07 17:10:58 +0800

    exercise 1.10

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,18 +4,25 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistics = ({ good, neutral, bad, all, average, positive }) => (
-  <div>
-    <h2>Statistics</h2>
-    <div>good {good}</div>
-    <div>neutral {neutral}</div>
-    <div>bad {bad}</div>
-    <div>all {all}</div>
-    <div>average {average}</div>
-    <div>positive {positive}</div>
-  </div>
-
+const Statistic = ({ text, value }) => (
+  <div>{text} {value}</div>
 )
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if (all === 0) {
+    return <div>No feedback given</div>
+  } else return (
+    <div>
+      <Statistic text={'good'} value={good} />
+      <Statistic text={'neutral'} value={neutral} />
+      <Statistic text={'bad'} value={bad} />
+      <Statistic text={'all'} value={all} />
+      <Statistic text={'average'} value={average} />
+      <Statistic text={'positive'} value={positive} />
+    </div>
+  )
+
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -33,6 +40,7 @@ const App = () => {
         <Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'} />
         <Button handleClick={() => setBad(bad + 1)} text={'bad'} />
       </div>
+      <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad}
         all={all} average={average} positive={positive} />
     </div>
