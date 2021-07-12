79b5583c03552c8a2d24952f9eaa5e0314dc7ce5
TravisWongX <travis.x.wong@gmail.com>
2021-07-12 13:39:23 +0800

    exercise 2.13

--- data_for_countries/src/App.js
+++ data_for_countries/src/App.js
@@ -8,26 +8,31 @@ const Languages = ({ lan }) => (
   </>
 )
 
-const ShowCountries = ({ countries, allData }) => {
+const CountryDetail = ({ name, allData }) => {
+  for (let item of allData) {
+    if (item.name === name) {
+      return (
+        <div>
+          <h1>{item.name}</h1>
+          <div>capital: {item.capital}</div>
+          <div>population: {item.population}</div>
+          <Languages lan={item.languages} />
+          <div><img src={item.flag} alt='flag' width="500" /></div>
+        </div>
+      )
+    }
+  }
+  return <></>
+}
+
+const ShowCountries = ({ countries, showCountry, allData }) => {
+  console.log(countries)
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length === 1) {
-    for (let item of allData) {
-      if (item.name === countries[0]) {
-        return (
-          <div>
-            <h1>{item.name}</h1>
-            <div>capital: {item.capital}</div>
-            <div>population: {item.population}</div>
-            <Languages lan={item.languages} />
-            <div><img src={item.flag} alt='flag' width="500" /></div>
-          </div>
-        )
-      }
-    }
-    return <div>hahaha</div>
+    return <CountryDetail name={countries[0]} allData={allData} />
   } else {
-    return countries.map(item => <div key={item}>{item}</div>)
+    return countries.map(item => <div key={item}>{item}<button onClick={showCountry}>show</button></div>)
   }
 }
 
@@ -35,6 +40,7 @@ const App = () => {
   const [allData, setAllData] = useState([])
   const [allCountries, setAllCountries] = useState([])
   const [countries, setCountries] = useState([])
+
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
@@ -49,10 +55,14 @@ const App = () => {
     setCountries(allCountries.filter(item => item.indexOf(event.target.value) >= 0))
   }
 
+  const showCountry = (event) => {
+    setCountries([event.target.parentNode.childNodes[0].textContent])
+  }
+
   return (
     <>
       <div>find countries: <input type='text' onChange={handleInputChange}></input></div>
-      <ShowCountries countries={countries} allData={allData} />
+      <ShowCountries countries={countries} allData={allData} showCountry={showCountry} />
     </>
   )
 }
