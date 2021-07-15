e09109689aabd296cbe1d364ac72226c26fb68a4
TravisWongX <travis.x.wong@gmail.com>
2021-07-15 16:50:19 +0800

    axios put

--- notes/src/App.js
+++ notes/src/App.js
@@ -41,7 +41,15 @@ const App = () => {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
   return (
     <div>
       <h1>Notes</h1>
@@ -52,7 +60,10 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
