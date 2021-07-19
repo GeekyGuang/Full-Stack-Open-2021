cb11ada8ffb45dabaefecb04537a945f08226ed6
TravisWongX <travis.x.wong@gmail.com>
2021-07-19 17:49:44 +0800

    post request

--- node/index.js
+++ node/index.js
@@ -1,5 +1,8 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -26,6 +29,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
@@ -34,6 +41,12 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
