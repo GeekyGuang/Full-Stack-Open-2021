b2f43faa7253addc6188f7e42f3c523a93a830fe
TravisWongX <travis.x.wong@gmail.com>
2021-07-20 09:48:44 +0800

    finish post

--- node/index.js
+++ node/index.js
@@ -43,9 +43,32 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+    console.log(body)
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+
     response.json(note)
 })
 
