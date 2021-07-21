817afbc23477821b09936e3dc35326a7ebebbf80
TravisWongX <travis.x.wong@gmail.com>
2021-07-21 10:53:14 +0800

    proxy

--- notes/src/services/notes.js
+++ notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://salty-sierra-05252.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
