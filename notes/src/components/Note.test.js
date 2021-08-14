d1b102673c251f4c43b40d77c4b036fdbba1b623
TravisWongX <travis.x.wong@gmail.com>
2021-08-14 10:11:40 +0800

    debug test

--- notes/src/components/Note.test.js
+++ notes/src/components/Note.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Note from './Note'
 
 test('renders content', () => {
@@ -11,7 +12,21 @@ test('renders content', () => {
 
   const component = render(<Note note={note} />)
 
+  const li = component.container.querySelector('li')
+  console.log(prettyDOM(li))
+
+  component.debug()
+
+  // method 1
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
+
+  // method 2
+  const element = component.getByText('Component testing is done with react-testing-library')
+  expect(element).toBeDefined()
+
+  // method 3
+  const div = component.container.querySelector('.note')
+  expect(div).toHaveTextContent('Component testing is done with react-testing-library')
 })
