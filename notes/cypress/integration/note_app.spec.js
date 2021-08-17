69dd44714dc6efdc570a964b2fe408206bdeef35
TravisWongX <travis.x.wong@gmail.com>
2021-08-17 11:32:49 +0800

    writing to a form

--- notes/cypress/integration/note_app.spec.js
+++ notes/cypress/integration/note_app.spec.js
@@ -1,6 +1,8 @@
 describe('Note app', function () {
-  it('from page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:3000')
+  })
+  it('from page can be opened', function () {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2021')
   })
@@ -9,4 +11,13 @@ describe('Note app', function () {
   //   cy.visit('http://localhost:3000')
   //   cy.contains('wtf is this app?')
   // })
+
+  it('login form can be opened', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Superuser logged-in')
+  })
 })
