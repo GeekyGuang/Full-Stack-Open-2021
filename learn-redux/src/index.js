ebd3f476c6a45848c3b883820ce34ef42e6271ba
TravisWongX <travis.x.wong@gmail.com>
2021-08-19 15:33:20 +0800

    使用redux实现计数器

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,4 +1,42 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div className="App">
+      {store.getState()}
+      <div>
+        <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
+          plus
+        </button>
+        <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
+          minus
+        </button>
+        <button onClick={() => store.dispatch({ type: 'ZERO' })}>zero</button>
+      </div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
