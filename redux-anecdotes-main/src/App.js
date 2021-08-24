2ce1f33b3d78a53914a447b93da8331a35fb5afd
TravisWongX <travis.x.wong@gmail.com>
2021-08-24 11:29:14 +0800

    use json-server

--- redux-anecdotes-main/src/App.js
+++ redux-anecdotes-main/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdotesService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { initAction } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then((anecdotes) => dispatch(initAction(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <Filter />
