57593aea89c1618a02fcc8e84b515455df33d752
TravisWongX <travis.x.wong@gmail.com>
2021-08-30 10:43:52 +0800

    country hook api

--- country-hook-main/src/App.js
+++ country-hook-main/src/App.js
@@ -11,14 +11,26 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      const queryUrl = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+      axios
+        .get(queryUrl)
+        .then((response) => {
+          setCountry({ found: true, data: response.data[0] })
+        })
+        .catch(() => {
+          setCountry({ found: false })
+        })
+    }
+  }, [name])
 
   return country
 }
@@ -29,19 +41,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
