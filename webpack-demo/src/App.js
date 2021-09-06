2bb91e13d1a6b735b66b6a933846151757a46173
TravisWongX <travis.x.wong@gmail.com>
2021-09-06 10:56:29 +0800

    config

--- webpack-demo/src/App.js
+++ webpack-demo/src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
+import axios from 'axios'
+import { response } from 'express'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then((response) => setNotes(response.data))
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -13,6 +24,9 @@ const App = () => {
     <div className="container">
       hello webpack {counter} clicks
       <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {BACKEND_URL}
+      </div>
     </div>
   )
 }
