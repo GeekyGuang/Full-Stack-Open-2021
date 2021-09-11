26a1bd61ac2353da7f7682a4ee6b8d12ce8eab68
TravisWongX <travis.x.wong@gmail.com>
2021-09-11 16:33:45 +0800

    exercise 9.11

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import diagnoseRouter from './routes/diagnoses'
+import patientRouter from './routes/patients'
 
 const app = express()
 app.use(express.json())
@@ -15,6 +16,7 @@ app.get('/api/ping', (_req, res) => {
 const PORT = 3001
 
 app.use('/api/diagnoses', diagnoseRouter)
+app.use('/api/patients', patientRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
