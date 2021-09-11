7498cc40e840db7e90d17b9ed3920d59da1a603b
TravisWongX <travis.x.wong@gmail.com>
2021-09-11 16:15:17 +0800

    exercise 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import diagnoseRouter from './routes/diagnoses'
+
 const app = express()
 app.use(express.json())
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -11,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
 })
 
 const PORT = 3001
+
+app.use('/api/diagnoses', diagnoseRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
