9ed00979e6c7f604a7d9e2819e5ab56392e8fc57
TravisWongX <travis.x.wong@gmail.com>
2021-09-13 17:47:46 +0800

    exercise 9.14

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface CoursePart {
+  name: string
+  exerciseCount: number
 }
 
-export default App;
+const Header = ({ courseName }: { courseName: string }) => <h1>{courseName}</h1>
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  return (
+    <>
+      {courseParts.map((item) => (
+        <p key={item.name}>
+          {item.name} {item.exerciseCount}
+        </p>
+      ))}
+    </>
+  )
+}
+const Total = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  return (
+    <p>
+      Number of exercises{''}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  )
+}
+
+const App: React.FC = () => {
+  const courseName = 'Half Stack application development'
+  const courseParts = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+    },
+  ]
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  )
+}
+
+export default App
