1f7a0b19862790fb3bfb7de9ac45204513d46c1b
TravisWongX <travis.x.wong@gmail.com>
2021-09-17 16:44:02 +0800

    deeper type

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-interface CoursePart {
+interface CoursePartBase {
   name: string
   exerciseCount: number
+  type: string
 }
+
+interface CourseNormalPart extends CoursePartBase {
+  type: 'normal'
+  description: string
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject'
+  groupProjectCount: number
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: 'submission'
+  description: string
+  exerciseSubmissionLink: string
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart
 
 const Header = ({ courseName }: { courseName: string }) => <h1>{courseName}</h1>
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
@@ -28,18 +47,31 @@ const Total = ({ courseParts }: { courseParts: CoursePart[] }) => {
 
 const App: React.FC = () => {
   const courseName = 'Half Stack application development'
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is the leisured course part',
+      type: 'normal',
+    },
+    {
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is the harded course part',
+      type: 'normal',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject',
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+      type: 'submission',
     },
   ]
 
